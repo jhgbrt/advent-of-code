fb012ebbc2f0833ec32dec9d8d32391b9f187fbc
Jeroen Haegebaert <jeroen@haegebaert.com>
2021-12-01 08:35:39 +0100

    2021 day 1

--- src/aoc/Year2021/Day01/AoC.cs
+++ src/aoc/Year2021/Day01/AoC.cs
@@ -1,8 +1,11 @@
+using Microsoft.FSharp.Collections;
+
 namespace AdventOfCode.Year2021.Day01;
 
 public class AoC202101 : AoCBase
 {
     static string[] input = Read.InputLines(typeof(AoC202101));
-    public override object Part1() => -1;
-    public override object Part2() => -1;
+    static int[] numbers = input.Select(int.Parse).ToArray();
+    public override object Part1() => numbers.Aggregate((previous: int.MaxValue, n: 0), (p, i) => (previous: i, n: p.n + (i > p.previous ? 1 : 0)));
+    public override object Part2() => SeqModule.Windowed(3, numbers).Select(window => window.Sum()).Aggregate((previous: int.MaxValue, n: 0), (p, i) => (previous: i, n: p.n + (i > p.previous ? 1 : 0)));
 }
