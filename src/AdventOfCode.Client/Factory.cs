0409144f1c30f570273e93d5745c0d043aadf9e3
Jeroen Haegebaert <jeroen@haegebaert.com>
2021-12-05 14:44:53 +0100

    use configuration system for injectiong AOC_SESSION

--- src/AdventOfCode.Client/Factory.cs
+++ src/AdventOfCode.Client/Factory.cs
@@ -1,10 +1,19 @@
-﻿namespace AdventOfCode.Client;
+﻿using Microsoft.Extensions.Configuration;
+
+using System.Reflection;
+
+namespace AdventOfCode.Client;
 
 static class Factory
 {
+    static IConfigurationRoot config = new ConfigurationBuilder()
+        .AddEnvironmentVariables()
+        .AddUserSecrets(Assembly.GetEntryAssembly())
+        .Build();
+
     public static AoCClient CreateClient()
     {
-        var cookieValue = Environment.GetEnvironmentVariable("AOC_SESSION") ?? throw new Exception("This operation requires AOC_SESSION to be set as an environment variable.");
+        var cookieValue = config["AOC_SESSION"] ?? throw new Exception("This operation requires AOC_SESSION to be set as an environment variable.");
         var baseAddress = new Uri("https://adventofcode.com");
         return new AoCClient(baseAddress, cookieValue);
     }
