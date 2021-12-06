26a6da38ae3de9a3c0997d7c712d7e247b6784c6
jhgbrt <jeroen@haegebaert.com>
2021-12-06 12:54:30 +0100

    update description

--- src/AdventOfCode.Client/Commands/Exec.cs
+++ src/AdventOfCode.Client/Commands/Exec.cs
@@ -11,24 +11,31 @@ class Exec : ICommand<Exec.Options>
 {
     public record Options(
         [property: Description("Year (default: current year)")] int? year,
-        [property: Description("Day (default: current day)")] int? day
+        [property: Description("Day (default: current day)")] int? day,
+        [property: Description("The fully qualified name of the type containing the code for this puzzle. " +
+        "Use a format string with {0} and {1} as placeholders for year and day. " +
+        "(default: AdventOfCode.Year{0}.Day{1:00}.AoC{0}{1:00})")] string? typeName
         ) : IOptions;
 
     public Task Run(Options options)
     {
 
-        (var year, var day) = (options.year??DateTime.Now.Year, options.day??DateTime.Now.Day);
+        (var year, var day, var typeName) = (
+              options.year??DateTime.Now.Year
+            , options.day??DateTime.Now.Day
+            , string.IsNullOrEmpty(options.typeName) ? "AdventOfCode.Year{0}.Day{1:00}.AoC{0}{1:00}" : options.typeName
+            );
 
         Console.WriteLine($"{year}, day {day}");
 
-        string typeName = $"AdventOfCode.Year{year}.Day{day:00}.AoC{year}{day:00}";
         var assembly = Assembly.GetEntryAssembly();
         if (assembly == null) throw new Exception("no entry assembly?");
-        var type = assembly.GetType(typeName);
+        Console.WriteLine(string.Format(typeName, year, day));
+        var type = assembly.GetType(string.Format(typeName, year, day));
 
         if (type is null)
         {
-            Console.WriteLine($"No implementation found for {year}, {day}");
+            Console.WriteLine($"Could not find type {typeName} for {year}, {day}. Use --typeName to override.");
             return Task.CompletedTask;
         }
 
