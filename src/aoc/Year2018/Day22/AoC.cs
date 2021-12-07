0919a3823541bd6ba6c3ba86138c5a3bad2edd15
jhgbrt <jeroen@haegebaert.com>
2021-12-07 19:38:53 +0100

    2018 22

--- src/aoc/Year2018/Day22/AoC.cs
+++ src/aoc/Year2018/Day22/AoC.cs
@@ -2,6 +2,39 @@ namespace AdventOfCode.Year2018.Day22;
 
 public class AoC201822 : AoCBase
 {
-    public override object Part1() => -1;
+    const int depth = 5616;
+    const int targetx = 10;
+    const int targety = 785;
+
+    public override object Part1() => Points().Select(p => RiskLevel(p)).Sum();
     public override object Part2() => -1;
+
+    IEnumerable<Point> Points()
+    {
+        for (int x = 0; x <= targetx; x++)
+            for (int y = 0; y <= targety; y++)
+                yield return new Point(x, y);
+    }
+
+    int RiskLevel(Point p) => ErosionLevel(p) % 3;
+
+    int GeologicIndex(Point p) => p switch
+    {
+        (0, 0) => 0,
+        (targetx, targety) => 0,
+        (_, 0) => p.x * 16807,
+        (0, _) => p.y * 48271,
+        _ => ErosionLevel(new(p.x - 1, p.y)) * ErosionLevel(new(p.x, p.y - 1))
+    };
+
+    Dictionary<Point, int> _cache = new();
+    private int ErosionLevel(Point p)
+    {
+        if (_cache.ContainsKey(p)) return _cache[p];
+        var result = (GeologicIndex(p) + depth) % 20183;
+        _cache[p] = result;
+        return result;
+    }
 }
+
+readonly record struct Point(int x, int y);
