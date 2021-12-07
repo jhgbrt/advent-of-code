bbb99910f48adcaf197eccb017073e624a4892cb
jhgbrt <jeroen@haegebaert.com>
2021-12-07 06:39:56 +0100

    day 7

---
+++ src/aoc/Year2021/Day07/AoC.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode.Year2021.Day07;
+
+public class AoC202107 : AoCBase
+{
+    static string input = Read.InputText(typeof(AoC202107));
+    static ImmutableArray<int> numbers = input.Split(',').Select(int.Parse).ToImmutableArray();
+    public override object Part1() => (from i in Range(0, numbers.Max())
+                                       let cost = (from j in numbers select Cost1(i,j)).Sum()
+                                       select cost).Min();
+
+    private static int Cost1(int x, int y) => Math.Abs(y - x);
+    private static int Cost2(int x, int y)
+    {
+        var distance = Cost1(x, y);
+        return distance * (distance + 1) / 2;
+    }
+
+    public override object Part2() => (from i in Range(0, numbers.Max())
+                                       let cost = (from j in numbers select Cost2(i, j)).Sum()
+                                       select cost).Min();
+}
