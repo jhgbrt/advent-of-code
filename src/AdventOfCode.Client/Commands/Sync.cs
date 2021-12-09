b04c7a39710237a21962679e1958d206020ab75d
jhgbrt <jeroen@haegebaert.com>
2021-12-09 20:47:45 +0100

    wip

--- src/AdventOfCode.Client/Commands/Sync.cs
+++ src/AdventOfCode.Client/Commands/Sync.cs
@@ -1,10 +1,10 @@
-﻿using System.ComponentModel;
-using System.Text.Json;
+﻿using Spectre.Console.Cli;
+using System.ComponentModel;
 
 namespace AdventOfCode.Client.Commands;
 
 [Description("Sync the data (specifically the posted answers) for a puzzle. Requires AOC_SESSION set as an environment variable.")]
-class Sync : ICommand<Sync.Options>
+class Sync : AsyncCommand<Sync.Settings>
 {
     private readonly AoCManager manager;
 
@@ -12,29 +12,37 @@ class Sync : ICommand<Sync.Options>
     {
         this.manager = manager;
     }
-    public record Options(
-        [property: Description("Year (default: current year)")] int? year,
-        [property: Description("Day (default: current day)")] int? day
-        ) : IOptions;
-    public async Task Run(Options options)
+
+    public class Settings : CommandSettings
+    {
+        [Description("Year (default: current year)")]
+        [CommandOption("-y|--year")]
+        public int? year { get; set; }
+        [Description("Day (default: current day)")]
+        [CommandOption("-d|--day")]
+        public int? day { get; set; }
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings options)
     {
         (var year, var day) = (options.year ?? DateTime.Now.Year, options.day ?? DateTime.Now.Day);
 
         if (!AoCLogic.IsValidAndUnlocked(year, day))
         {
             Console.WriteLine("Puzzle not yet unlocked");
-            return;
+            return 1;
         }
 
         var dir = FileSystem.GetDirectory(year, day);
         if (!dir.Exists)
         {
             Console.WriteLine("Puzzle not yet initialized. Use 'init' first.");
-            return;
+            return 1;
         }
 
         Console.WriteLine("Updating puzzle answers");
         await manager.Sync(year, day);
+        return 0;
     }
-
+   
 }
