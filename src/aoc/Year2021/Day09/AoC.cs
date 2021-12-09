66a0feacf106775358bc715554a65061b9988d43
jhgbrt <jeroen@haegebaert.com>
2021-12-09 08:42:36 +0100

    more linq

--- src/aoc/Year2021/Day09/AoC.cs
+++ src/aoc/Year2021/Day09/AoC.cs
@@ -2,54 +2,58 @@ namespace AdventOfCode.Year2021.Day09;
 
 public class AoC202109 : AoCBase
 {
-    static string[] input = Read.InputLines(typeof(AoC202109));
-    static Grid grid = new Grid(input);
-    public override object Part1() => (from p in grid.Points()
-                                       let value = grid[p]
-                                       where grid.Neighbours(p).All(n => value < grid[n])
-                                       select value + 1).Sum();
+    static Grid grid = new(Read.InputLines(typeof(AoC202109)));
+
+    public override object Part1() => (
+        from p in grid.Points()
+        let value = grid[p]
+        where grid.Neighbours(p).All(n => value < grid[n])
+        select value + 1
+        ).Sum();
 
     public override object Part2() => (
-            from low in 
-                from p in grid.Points()
-                let neighbours = grid.Neighbours(p)
-                let value = grid[p]
-                where neighbours.All(n => value < grid[n])
-                select p
-            let size = grid.Basin(low).Distinct().Count()
-            orderby size descending
-            select size
-            ).Take(3).Aggregate(1, (a, i) => i * a);
+        from low in 
+            from p in grid.Points()
+            let neighbours = grid.Neighbours(p)
+            let value = grid[p]
+            where neighbours.All(n => value < grid[n])
+            select p
+        let size = grid.Basin(low).Distinct().Count()
+        orderby size descending
+        select size
+        ).Take(3).Aggregate(1, (a, i) => i * a);
 }
 class Grid
 {
     readonly string[] input;
-    readonly Point origin = new Point(0, 0);
+    readonly Point origin = new (0, 0);
     readonly Point length;
     public Grid(string[] input)
     {
         this.input = input;
-        this.length = new Point(input[0].Length, input.Length);
+        this.length = new (input[0].Length, input.Length);
     }
     public int this[Point p] => input[p.y][p.x] - '0';
     public IEnumerable<Point> Basin(Point p)
     {
         yield return p;
-        foreach (var n in Neighbours(p).Where(n => this[n] > this[p] && this[n] < 9))
-        {
-            foreach (var x in Basin(n)) yield return x;
-        }
+
+        var q = from n in Neighbours(p)
+                where this[n] > this[p] && this[n] < 9
+                from b in Basin(n)
+                select b;
+
+        foreach (var n in q) yield return n;
     }
 
-    public IEnumerable<Point> Points() => from x in Range(origin.x, length.x)
-                                          from y in Range(origin.y, length.y)
-                                          select new Point(x, y);
-    public IEnumerable<Point> Neighbours(Point p)
-    {
-        if (p.x > origin.x) yield return p with { x = p.x - 1 };
-        if (p.y < length.y - 1) yield return p with { y = p.y + 1 };
-        if (p.y > origin.y) yield return p with { y = p.y - 1 };
-        if (p.x < length.x - 1) yield return p with { x = p.x + 1 };
-    }
+    public IEnumerable<Point> Points() =>
+        from x in Range(origin.x, length.x)
+        from y in Range(origin.y, length.y)
+        select new Point(x, y);
+
+    public IEnumerable<Point> Neighbours(Point p) =>
+        from d in new (int x, int y)[] { (-1, 0), (0, 1), (1, 0), (0, -1) }
+        where (p.x + d.x, p.y + d.y) is ( >= 0 and < 100, >= 0 and < 100)
+        select new Point(p.x + d.x, p.y + d.y);
 }
 readonly record struct Point(int x, int y);
