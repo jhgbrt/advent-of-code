f1e223be7195b677461bd900a190abb9c0272a2d
jhgbrt <jeroen@haegebaert.com>
2021-12-11 06:03:02 +0100

    WIP

---
+++ src/aoc/Year2021/Day11/AoC.cs
@@ -0,0 +1,8 @@
+namespace AdventOfCode.Year2021.Day11;
+
+public class AoC202111 : AoCBase
+{
+    static string[] input = Read.InputLines(typeof(AoC202111));
+    public override object Part1() => -1;
+    public override object Part2() => -1;
+}
