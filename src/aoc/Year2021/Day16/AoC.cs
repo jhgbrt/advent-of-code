42f23041632fe0b179f842f4f8b5e262e58f31f5
jhgbrt <jeroen@haegebaert.com>
2021-12-26 01:27:34 +0100

    cleanup

--- src/aoc/Year2021/Day16/AoC.cs
+++ src/aoc/Year2021/Day16/AoC.cs
@@ -5,351 +5,108 @@ namespace AdventOfCode.Year2021.Day16;
 public class AoC202116
 {
     static string input = Read.InputLines()[0];
-    public object Part1()
-    {
-        var binary = input.ToBinary();
-        var packet = new Packet2(binary);
-        return packet.GetVersionSum();
+    public object Part1() => new Packet(input.ToBinary()).GetVersionSum();
 
-
-        //var packets = binary.GetEnumerator().DecodePackets();
-
-        //return 0;
-    }
-          
-    public object Part2()
-    {
-        var binary = input.ToBinary();
-        Packet2 packet = new Packet2(binary);
-        return packet.GetValue();
-    }
-
-
-    
-
+    public object Part2() => new Packet(input.ToBinary()).Value;
 }
 
-static class Decoder
+static class Ex
 {
     public static string ToBinary(this string input) => string.Join(string.Empty, input.Select(c => Convert.ToString(
         Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0'))
         );
-
-    static int Read(IEnumerator<char> enumerator, StringBuilder sb, int n)
-    {
-        sb.Clear();
-        int i = 0;
-        while (i < n && enumerator.MoveNext())
-        {
-            sb.Append(enumerator.Current);
-            i++;
-        }
-        return i;
-    }
-
-    static int ReadLiteral(IEnumerator<char> enumerator, StringBuilder sb)
-    {
-        sb.Clear();
-        StringBuilder inner = new StringBuilder();
-        bool done = false;
-        int read = 0;
-        while (!done)
-        {
-            read += Read(enumerator, inner, 5);
-            done = inner[0] == '0';
-            sb.Append(inner.Remove(0, 1));
-        }
-        return read;
-    }
-
-    static int ReadSubPacketBits(IEnumerator<char> enumerator, StringBuilder sb)
-    {
-        return 0;
-    }
-    static IEnumerable<IPacket> ReadSubPackets(IEnumerator<char> enumerator, StringBuilder sb, int n)
-    {
-        int i = 0;
-        while (i < n)
-        {
-
-            i++;
-        }
-        yield break;
-    }
-
-    static int ReadInt(IEnumerator<char> enumerator, int n)
-    {
-        var sb = new StringBuilder();
-        Read(enumerator, sb, n);
-        return Convert.ToInt32(sb.ToString(), 2);
-    }
-
-    public static IEnumerable<IPacket> DecodePackets(this IEnumerator<char> binary)
-    {
-        var sb = new StringBuilder();
-
-        for (int x = 0; x<100; x++)
-        {
-            int read = 0;
-            
-            var v = ReadInt(binary, 3);
-            var id = ReadInt(binary, 3);
-
-            switch (id)
-            {
-                case 4:
-                    read = ReadLiteral(binary, sb);
-                    if (read == 0) yield break;
-                    var literal = new Literal(id, v, Convert.ToInt32(sb.ToString(), 2));
-                    yield return literal;
-                    break;
-                case 6:
-                    Read(binary, sb, 1);
-                    switch (sb[0])
-                    {
-                        case '1':
-                            {
-                                Read(binary, sb, 11);
-                                var n = Convert.ToInt32(sb.ToString(), 2);
-                                //yield return new Packet { SubPackets = ReadSubPackets(binary, n) };
-                            }
-                            break;
-                        case '0':
-                            {
-                                Read(binary, sb, 15);
-                                var n = Convert.ToInt32(sb.ToString(), 2);
-
-                                //int bytesRead = 0;
-                                //while (bytesRead < lengthOfSubpackets)
-                                //{
-                                //    Packet2 subpacket = new Packet2(bitStream);
-                                //    subpackets.Add(subpacket);
-
-                                //    bitStream = bitStream.Skip(subpacket.bytesRead);
-                                //    bytesRead += subpacket.bytesRead;
-                                //}
-                                //return bytesRead;
-                                
-                                //index++;
-                                //var length = 15;
-                                //var subpacketsLength = Convert.ToInt16(binary[index..(index + length)], 2);
-                                //index += length;
-                                //Assert.Equal(27, subpacketsLength);
-                                //var subpackets = binary[index..(index+ subpacketsLength)];
-                                //Assert.Equal("110100010100101001000100100", subpackets);
-                                //yield return new Packet { SubPackets = subpackets.DecodePackets().ToList() };
-                                //index += subpacketsLength;
-                            }
-                            break;
-                        default:
-                            throw new Exception();
-                    }
-
-                    break;
-
-            }
-
-        }
-    }
-
-    public static Literal Literal(this string s, int id, int version, Range r)
-    {
-        return new Literal(
-            id,
-            version,
-            Convert.ToInt32(s[r][1..5] + s[r][6..10] + s[r][11..15], 2)
-            );
-    }
-}
-interface IPacket { }
-class Packet : IPacket
-{
-    public List<IPacket>? SubPackets { get; set; }
 }
 
-record Literal(int id, int version, int value) : IPacket;
 
-public class Tests
+
+
+internal class Packet
 {
-    [Fact]
-    public void Test1()
-    {
-        //var literal = "D2FE28";
-        //var p = new Packet2(literal.ToBinary());
-        //var binary = literal.ToBinary();
-        //var decoded = binary.GetEnumerator().DecodePackets().Single();
-        //Assert.True(decoded is Literal);
-        //var l = (Literal)decoded;
-        //Assert.Equal(4, l.id);
-        //Assert.Equal(6, l.version);
-        //Assert.Equal(2021, l.value);
-    }
-
-    [Fact]
-    public void Test2()
-    {
-        //var hex = "38006F45291200";
-        //var p = new Packet2(hex.ToBinary());
-        //var binary = hex.ToBinary();
-        //var decoded = binary.GetEnumerator().DecodePackets().ToList();
-        //var count = decoded.Count();
-        //Assert.Equal(1, count);
-        //var pa = decoded.Single();
-        //Assert.Equal(2, ((Packet)pa).SubPackets.Count);
-    }
-
-}
-
-class PacketDecoder 
-{
-    private IEnumerable<char> bitStream;
-
-    public PacketDecoder(IEnumerable<char> bitStream)
-    {
-        this.bitStream = bitStream;
-    }
-
-    private int bytesRead;
-    public IEnumerable<char> ReadStream(int amount)
-    {
-        IEnumerable<char> result = bitStream.Take(amount);
-        bytesRead += amount;
-        bitStream = bitStream.Skip(amount);
-        return result;
-    }
-
-}
-
-internal class Packet2
-{
-    private int bytesRead;
-    private List<Packet2> SubPackets { get; }
+    private int Length;
+    public IEnumerable<Packet> Children => children;
+    private List<Packet> children { get; } = new();
     private int version;
-    private TypeID typeID;
-    private long value = long.MinValue;
-    private IEnumerable<char> bitStream;
-    private PacketDecoder decoder;
-    public Packet2(IEnumerable<char> _bitStream)
+    private int type;
+    private string binary;
+    public Packet(string binary)
     {
-        decoder = new PacketDecoder(_bitStream);
-        bitStream = _bitStream;
-        SubPackets = new();
+        this.binary = binary;
 
-        // Read header
-        version = Convert.ToInt32(new string(ReadStream(3).ToArray()), 2);
-        typeID = (TypeID)Convert.ToInt32(new string(ReadStream(3).ToArray()), 2);
+        // header
+        version = Convert.ToInt32(Read(3).ToString(), 2);
+        type = Convert.ToInt32(Read(3).ToString(), 2);
 
-        // Parse data
-        if (typeID == TypeID.LITERAL)
+        // data
+        if (type == 4)
         {
             StringBuilder result = new();
             bool readLastGroup = false;
             while (!readLastGroup)
             {
-                if (ReadStream(1).First() == '0')
+                if (Read(1)[0] == '0')
                     readLastGroup = true;
-                result.Append(ReadStream(4).ToArray());
+                result.Append(Read(4));
             }
-            value = Convert.ToInt64(result.ToString(), 2);
+            Value = Convert.ToInt64(result.ToString(), 2);
         }
         else
         {
-            if (ReadStream(1).First() == '0') // Extract subpackets by length
-            {
-                int lengthOfSubpackets = Convert.ToInt32(new string(ReadStream(15).ToArray()), 2);
-                bytesRead += ExtractSubpacketsByLength(lengthOfSubpackets, bitStream);
-            }
-            else // Extract subpackets by number
-            {
-                int numberOfSubpackets = Convert.ToInt32(new string(ReadStream(11).ToArray()), 2);
-                bytesRead += ExtractSubpacketsByNumber(numberOfSubpackets, bitStream);
-            }
 
-            // Get value based on subvalues
-            var subValues = SubPackets.Select(packet => packet.value);
-            switch (typeID)
+            var subpackets = (Read(1)[0] switch
             {
-                case TypeID.SUM:
-                    value = subValues.Sum();
-                    break;
-                case TypeID.PRODUCT:
-                    value = subValues.Aggregate((long)1, (x, y) => x * y);
-                    break;
-                case TypeID.MINIMUM:
-                    value = subValues.Min();
-                    break;
-                case TypeID.MAXIMUM:
-                    value = subValues.Max();
-                    break;
-                case TypeID.GREATER_THAN:
-                    value = subValues.First() > subValues.Last() ? 1 : 0;
-                    break;
-                case TypeID.LESS_THAN:
-                    value = subValues.First() < subValues.Last() ? 1 : 0;
-                    break;
-                case TypeID.EQUALS:
-                    value = subValues.First() == subValues.Last() ? 1 : 0;
-                    break;
-                default:
-                    throw new NotImplementedException();
-            }
+                '0' => ReadNumberOfBits(Convert.ToInt32(Read(15).ToString(), 2)),
+                _ => ReadNumberOfPackets(Convert.ToInt32(Read(11).ToString(), 2))
+            }).ToList();
+
+            children.AddRange(subpackets);
+            var subValues = subpackets.Select(packet => packet.Value);
+            Value = type switch
+            {
+                0 => subValues.Sum(),
+                1 => subValues.Aggregate((long)1, (x, y) => x * y),
+                2 => subValues.Min(),
+                3 => subValues.Max(),
+                4 => Value,
+                5 => subValues.First() > subValues.Last() ? 1 : 0,
+                6 => subValues.First() < subValues.Last() ? 1 : 0,
+                7 => subValues.First() == subValues.Last() ? 1 : 0,
+                _ => throw new NotImplementedException(),
+            };
         }
     }
-    public long GetValue() => value;
-    public int GetVersionSum()
+    public long Value { get; set; }
+    public int GetVersionSum() => children.Aggregate(version, (s, sub) => s + sub.GetVersionSum());
+
+    private ReadOnlySpan<char> Read(int amount)
     {
-        int sum = version;
-        foreach (var subpacket in SubPackets)
+        var span = binary.AsSpan(Length, amount);
+        Length += amount;
+        return span;
+    }
+
+    private IEnumerable<Packet> ReadNumberOfPackets(int number)
+    {
+        var str = binary.Substring(Length);
+        for (int i = 0; i < number; i++)
         {
-            sum += subpacket.GetVersionSum();
+            var subpacket = new Packet(str);
+            yield return subpacket;
+            str = str.Substring(subpacket.Length);
+            Length += subpacket.Length;
         }
-        return sum;
-    }
-    private IEnumerable<char> ReadStream(int amount)
-    {
-        IEnumerable<char> result = bitStream.Take(amount);
-        bytesRead += amount;
-        bitStream = bitStream.Skip(amount);
-        return result;
     }
 
-    private int ExtractSubpacketsByNumber(int numberOfSubpackets, IEnumerable<char> bitStream)
+    private IEnumerable<Packet> ReadNumberOfBits(int length)
     {
-        int packetsAdded = 0;
-        while (packetsAdded++ < numberOfSubpackets)
-        {
-            SubPackets.Add(new Packet2(bitStream));
-            bitStream = bitStream.Skip(SubPackets.Last().bytesRead);
-
-        }
-
-        return SubPackets.Select(packet => packet.bytesRead).Sum();
-    }
-
-    private int ExtractSubpacketsByLength(int lengthOfSubpackets, IEnumerable<char> bitStream)
-    {
+        string str = binary.Substring(Length);
         int bytesRead = 0;
-        while (bytesRead < lengthOfSubpackets)
+        while (bytesRead < length)
         {
-            Packet2 subpacket = new Packet2(bitStream);
-            SubPackets.Add(subpacket);
-
-            bitStream = bitStream.Skip(subpacket.bytesRead);
-            bytesRead += subpacket.bytesRead;
+            Packet subpacket = new Packet(str);
+            yield return subpacket;
+            str = str.Substring(subpacket.Length);
+            Length += subpacket.Length;
+            bytesRead += subpacket.Length;
         }
-        return bytesRead;
     }
-}
-
-enum TypeID
-{
-    SUM = 0,
-    PRODUCT = 1,
-    MINIMUM = 2,
-    MAXIMUM = 3,
-    LITERAL = 4,
-    GREATER_THAN = 5,
-    LESS_THAN = 6,
-    EQUALS = 7
-
 }
