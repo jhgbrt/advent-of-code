23d83912ee21ebab17d67e34aab474a0cd8e5000
jhgbrt <jeroen@haegebaert.com>
2021-12-28 22:45:15 +0100

    wip

---
+++ src/AdventOfCode.Client/Logic/CodeFolder.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AdventOfCode.Client.Logic;
+
+class CodeFolder
+{
+    DirectoryInfo dir;
+    int year;
+    int day;
+    private readonly ILogger logger;
+
+    public CodeFolder(int year, int day, ILogger logger)
+    {
+        this.dir = GetDirectory(year, day);
+        this.year = year;
+        this.day = day;
+        this.logger = logger;
+    }
+
+    private string CODE => GetFileName(year, day, "AoC.cs");
+    private string INPUT => GetFileName(year, day, "input.txt");
+    private string SAMPLE => GetFileName(year, day, "sample.txt");
+
+    private Task<string> ReadFile(string name)
+    {
+        logger.LogTrace($"READ: {name}");
+        return File.ReadAllTextAsync(name);
+    } 
+    private Task WriteFile(string name, string content)
+    {
+        logger.LogTrace($"WRITE: {name}");
+        return File.WriteAllTextAsync(name, content);
+    }
+
+    public Task<string> ReadCode() => ReadFile(CODE);
+    public Task WriteCode(string content) => WriteFile(CODE, content);
+    public Task<string> ReadInput() => ReadFile(INPUT);
+    public Task WriteInput(string content) => WriteFile(INPUT, content);
+    public Task<string> ReadSample() => ReadFile(SAMPLE);
+    public Task WriteSample(string content) => WriteFile(SAMPLE, content);
+    public bool Exists => dir.Exists;
+    public void Create()
+    {
+        logger.LogTrace($"CREATE: {dir}");
+        dir.Create();
+    }
+
+    public void Delete()
+    {
+        logger.LogTrace($"DELETE: {dir}");
+        dir.Delete(true);
+    }
+
+    internal async Task ExportTo(string code, string output)
+    {
+        var publishLocation = new DirectoryInfo(output);
+        if (!publishLocation.Exists)
+        {
+            logger.LogTrace($"CREATE: {publishLocation}");
+            publishLocation.Create();
+        }
+
+        var aoccs = Path.Combine(publishLocation.FullName, "aoc.cs");
+        if (File.Exists(aoccs))
+        {
+            logger.LogTrace($"DELETE: {aoccs}");
+            File.Delete(aoccs);
+        }
+
+        logger.LogTrace($"WRITE: {aoccs}");
+        await File.WriteAllTextAsync(aoccs, code);
+
+        foreach (var file in dir.GetFiles("*.cs").Where(f => !f.FullName.Equals(CODE, StringComparison.OrdinalIgnoreCase)))
+        {
+            logger.LogTrace($"COPY: {file.FullName} -> {Path.Combine(publishLocation.FullName, file.Name)}");
+            file.CopyTo(Path.Combine(publishLocation.FullName, file.Name), true);
+        }
+
+        var inputtxt = Path.Combine(publishLocation.FullName, "input.txt");
+        logger.LogTrace($"COPY: {INPUT} -> {inputtxt}");
+        File.Copy(INPUT, inputtxt, true);
+
+        var csprojsrc = Path.Combine(Environment.CurrentDirectory, "Template", "aoc.csproj");
+        var csprojdest = Path.Combine(publishLocation.FullName, "aoc.csproj");
+        logger.LogTrace($"COPY: {csprojsrc} -> {csprojdest}");
+        File.Copy(csprojsrc, csprojdest, true);
+    }
+
+    static DirectoryInfo GetDirectory(int year, int day) => new(Path.Combine(Environment.CurrentDirectory, $"Year{year}", $"Day{day:00}"));
+    static FileInfo GetFile(int year, int day, string fileName) => new(Path.Combine(Environment.CurrentDirectory, $"Year{year}", $"Day{day:00}", fileName));
+    static string GetFileName(int year, int day, string fileName) => GetFile(year, day, fileName).FullName;
+}
