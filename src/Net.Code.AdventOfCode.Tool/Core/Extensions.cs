1182a1c33dcb43264ced1d84329abe0a6c4412a1
jhgbrt <jeroen@haegebaert.com>
2022-01-14 18:35:22 +0100

    remove unused code

--- src/Net.Code.AdventOfCode.Tool/Core/Extensions.cs
+++ src/Net.Code.AdventOfCode.Tool/Core/Extensions.cs
@@ -5,13 +5,6 @@ namespace Net.Code.AdventOfCode.Tool.Core;
 
 public static class Extensions
 {
-    public static string ToHumanReadableString(this TimeSpan t) => t switch
-    {
-        { TotalSeconds: < 1 } => $"{t.Milliseconds} ms",
-        { TotalMinutes: < 1 } => $@"{t:s\.f} s",
-        { TotalHours: < 1 } => $@"{t:mm\:ss} m",
-        _ => t.ToString()
-    };
     public static string GetDisplayName<T>(this T e) where T : Enum
     {
         return typeof(T).GetMember(e.ToString()).First().GetCustomAttribute<DisplayAttribute>()?.GetName() ?? e.ToString();
