c4538c704df9c54a91ae98fae1c3b24e070480a6
jhgbrt <jeroen@haegebaert.com>
2022-12-07 10:36:02 +0100

    cleanup

--- src/AdventOfCode/Year2022/Day07/AoC.cs
+++ src/AdventOfCode/Year2022/Day07/AoC.cs
@@ -1,44 +1,33 @@
-using Sprache;
-
-using System.IO;
-
 namespace AdventOfCode.Year2022.Day07;
 
 public class AoC202207
 {
     static string[] input = Read.InputLines();
+    static readonly Directory Root = Directory.Parse(input);
 
-    public object Part1() => Directory.Parse(input).AllChildren().Where(d => d.Size < 100000).Sum(d => d.Size);
+    public long Part1() => (from d in Root.AllChildren()
+                            where d.Size < 100000
+                            select d.Size).Sum();
 
-
-
-    public object Part2()
-    {
-        var root = Directory.Parse(input);
-        var total = 70000000L;
-        var required = 30000000L;
-        var taken = root.Size;
-        var unused = total - taken;
-        var tofree = required - unused;
-        var todelete = root
-            .AllChildren()
-            .Where(d => d.Size >= tofree)
-            .OrderBy(d => d.Size).First();
-        return todelete.Size;
-    }
+    public long Part2() => (from d in Root.AllChildren()
+                            where d.Size >= (Root.Size - 40000000)
+                            orderby d.Size
+                            select d).First().Size;
 }
 
 abstract class FileSystemEntry
 {
-    public string Name { get; init; }
+    public FileSystemEntry(string name) => Name = name;
+    public string Name { get; }
     public List<FileSystemEntry> Children { get; } = new();
     public abstract long Size { get; }
 }
-class Directory : FileSystemEntry
+
+partial class Directory : FileSystemEntry
 {
-    static Regex FileRegex = new(@"^(?<size>\d+) (?<name>.+)$");
-    static Regex DirectoryRegex = new(@"^dir (?<name>.+)$");
-    public static Directory Parse(string[] input)
+    static Regex FileRegex = CreateFileRegex();
+    static Regex DirectoryRegex = CreateDirectoryRegex();
+    public static Directory Parse(IEnumerable<string> input)
     {
         var root = new Directory("/", null);
         var cd = root;
@@ -59,18 +48,14 @@ class Directory : FileSystemEntry
     }
     public IEnumerable<Directory> AllChildren()
     {
-        yield return this;
         foreach (var child in Children.OfType<Directory>())
         {
             foreach (var d in child.AllChildren())
                 yield return d;
         }
+        yield return this;
     }
-    private Directory(string name, Directory? parent)
-    {
-        Name = name;
-        Parent = parent;
-    }
+    private Directory(string name, Directory? parent) : base(name) => Parent = parent;
     public Directory? Parent { get; }
     public override long Size => Children.Sum(f => f.Size);
     public Directory AddFile(string name, long size)
@@ -82,7 +67,7 @@ class Directory : FileSystemEntry
     public Directory AddDirectory(string name)
     {
         Children.Add(new Directory(name, this));
-        return this;;
+        return this;
     }
 
     public Directory Find(string name) => name switch
@@ -92,15 +77,14 @@ class Directory : FileSystemEntry
     };
 
     public override string ToString() => $"dir {Name}";
-
+    [GeneratedRegex("^(?<size>\\d+) (?<name>.+)$", RegexOptions.Compiled)]
+    private static partial Regex CreateFileRegex();
+    [GeneratedRegex("^dir (?<name>.+)$", RegexOptions.Compiled)]
+    private static partial Regex CreateDirectoryRegex();
 }
 class File : FileSystemEntry
 {
-    public File(string name, long size)
-    {
-        Name = name;
-        Size = size;
-    }
+    public File(string name, long size) : base(name) => Size = size;
     public override long Size { get; }
     public override string ToString() => $"{Size} {Name}";
 }
