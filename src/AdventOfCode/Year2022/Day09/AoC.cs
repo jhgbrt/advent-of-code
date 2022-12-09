1256987560fd19af8b8715d9e4c6c22e41fc4d2c
jhgbrt <jeroen@haegebaert.com>
2022-12-10 00:16:12 +0100

    day 9

--- src/AdventOfCode/Year2022/Day09/AoC.cs
+++ src/AdventOfCode/Year2022/Day09/AoC.cs
@@ -1,24 +1,23 @@
-using AdventOfCode.Year2022.Day05;
+using static System.Math;
 
 namespace AdventOfCode.Year2022.Day09;
 public class AoC202209
 {
-    static string[] input = Read.SampleLines();
+    static string[] input = Read.InputLines();
     private IEnumerable<char> movements = from line in input
-                                                            let d = line[0]
-                                                            let l = int.Parse(line[2..])
-                                                            from n in Repeat(1, l)
-                                                            select d;
-    public int Part1() 
+                                          let d = line[0]
+                                          let l = int.Parse(line[2..])
+                                          from n in Repeat(1, l)
+                                          select d;
+    public int Part1()
     {
         Point h = new(), t = new();
 
-        HashSet<Point> visited = new();
-        visited.Add(t);
+        HashSet<Point> visited = new() { t };
         foreach (var d in movements)
         {
             h = h.Move(d);
-            t = t.MoveTo(h, d);
+            t = t.MoveTo(h);
             visited.Add(t);
         }
         return visited.Count;
@@ -27,26 +26,22 @@ public class AoC202209
 
     public int Part2()
     {
-        Point h = new(), t1 = new(), t2 = new(), t3 = new(), t4 = new(), t5 = new(), t6 = new(), t7 = new(), t8 = new(), t9 = new();
+        Point[] knots = Repeat(new Point(0, 0), 10).ToArray();
+        HashSet<Point> visited = new()
+        {
+            knots.Last()
+        };
 
-        HashSet<Point> visited = new();
-        visited.Add(t9);
         foreach (var d in movements)
         {
-            Console.WriteLine(d);
-            h = h.Move(d);
-            t1 = t1.MoveTo(h, d);
-            t2 = t2.MoveTo(t1, d);
-            t3 = t3.MoveTo(t2, d);
-            t4 = t4.MoveTo(t3, d);
-            t5 = t5.MoveTo(t4, d);
-            t6 = t6.MoveTo(t5, d);
-            t7 = t7.MoveTo(t6, d);
-            t8 = t8.MoveTo(t7, d);
-            t9 = t9.MoveTo(t8, d);
-            visited.Add(t9);
-            Console.WriteLine(t9);
+            knots[0] = knots[0].Move(d);
+            for (int i = 1; i < knots.Length; i++)
+            {
+                knots[i] = knots[i].MoveTo(knots[i - 1]);
+            }
+            visited.Add(knots[9]);
         }
+
         return visited.Count;
     }
 }
@@ -55,51 +50,24 @@ readonly record struct Point(int x, int y)
 {
     public Point Move(char d) => d switch
     {
-        'R' => this with { x = x + 1 },
-        'L' => this with { x = x - 1 },
-        'U' => this with { y = y + 1 },
-        'D' => this with { y = y - 1 },
+        'R' => Move(1, 0),
+        'L' => Move(-1, 0),
+        'U' => Move(0, 1),
+        'D' => Move(0, -1),
         _ => throw new Exception("unexpected input")
     };
-    public Point MoveTo(Point h, char d) => Toofar(h) ? d switch
-    {
-        'R' => this with { x = h.x > x ? h.x - 1 : h.x + 1, y = h.y },
-        'L' => this with { x = h.x < x ? h.x + 1 : h.x - 1, y = h.y },
-        'U' => this with { x = h.x, y = h.y > y ? h.y - 1 : h.y},
-        'D' => this with { x = h.x, y = h.y < y ? h.y + 1 : h.y},
-        _ => throw new Exception("unexpected input")
-    } : this;
+    public Point Move(int dx, int dy) => this with { x = x + dx, y = y + dy };
 
-    private bool Toofar(Point other)
-        => Math.Abs(other.x - x) > 1 || Math.Abs(other.y - y) > 1;
+    public Point MoveTo(Point h) => Distance(h) switch
+    {
+        > 1 => Move(
+                (h.x - x) switch { > 0 => 1, < 0 => -1, 0 => 0 },
+                (h.y - y) switch { > 0 => 1, < 0 => -1, 0 => 0 }
+                ),
+        _ => this
+    };
+    private int Distance(Point other)
+        => Max(Abs(other.x - x), Abs(other.y - y));
 
     public override string ToString() => $"({x},{y})";
-}
-
-public class Tests
-{
-    [Theory]
-    [InlineData(0, 0, 0, 0, 'R', 4, 4, 0, 3, 0)]
-    [InlineData(4, 0, 3, 0, 'U', 4, 4, 4, 4, 3)]
-    [InlineData(4, 4, 4, 3, 'L', 3, 1, 4, 2, 4)]
-    [InlineData(1, 4, 2, 4, 'D', 1, 1, 3, 2, 4)]
-    [InlineData(1, 3, 2, 4, 'R', 4, 5, 3, 4, 3)]
-    [InlineData(5, 3, 4, 3, 'D', 1, 5, 2, 4, 3)]
-    [InlineData(5, 2, 4, 3, 'L', 5, 0, 2, 1, 2)]
-    [InlineData(0, 2, 1, 2, 'R', 2, 2, 2, 1, 2)]
-    public void Test(int hx1, int hy1, int tx1, int ty1, char d, int l, int hx2, int hy2, int tx2, int ty2)
-    {
-        var h = new Point(hx1, hy1);
-        var t = new Point(tx1, ty1);
-
-        var expected = (h: new Point(hx2, hy2), t: new Point(tx2, ty2));
-
-        foreach (var i in Enumerable.Repeat(1, l))
-        {
-            h = h.Move(d);
-            t = t.MoveTo(h, d);
-        }
-
-        Assert.Equal(expected, (h, t));
-    }
 }
