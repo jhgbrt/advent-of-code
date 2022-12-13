cec331c3673229673a4c5e6f6d2f928aac018788
jhgbrt <jeroen@haegebaert.com>
2022-12-13 17:00:44 +0100

    day 13

---
+++ src/AdventOfCode/Common/LinqExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Common;
+
+static class LinqExtensions
+{
+    public static IEnumerable<(T a, T? b)> PairWise<T>(this IEnumerable<T> list)
+    {
+        var enumerator = list.GetEnumerator();
+        while (true)
+        {
+            if (!enumerator.MoveNext()) yield break;
+            var a = enumerator.Current;
+            if (!enumerator.MoveNext())
+            {
+                yield return (a, default);
+                yield break;
+            }
+            var b = enumerator.Current;
+            yield return (a, b);
+        }
+    }
+    public static IEnumerable<(T a, T? b, T? c)> TripletWise<T>(this IEnumerable<T> list)
+    {
+        var enumerator = list.GetEnumerator();
+        while (true)
+        {
+            if (!enumerator.MoveNext()) yield break;
+            var a = enumerator.Current;
+            if (!enumerator.MoveNext())
+            {
+                yield return (a, default, default);
+                yield break;
+            }
+            var b = enumerator.Current;
+            if (!enumerator.MoveNext())
+            {
+                yield return (a, b, default);
+                yield break;
+            }
+            var c = enumerator.Current;
+            yield return (a, b, c);
+        }
+    }
+}
--- src/AdventOfCode/Year2022/Day13/AoC.cs
+++ src/AdventOfCode/Year2022/Day13/AoC.cs
@@ -1,64 +1,53 @@
-using Newtonsoft.Json.Linq;
+using AdventOfCode.Common;
 
 namespace AdventOfCode.Year2022.Day13;
 public class AoC202213
 {
     static string[] input = Read.InputLines();
-    static IComparer<JToken> comparer = new JTokenComparer();
+    static IComparer<JsonElement> comparer = new MyJsonArrayComparer();
     public int Part1() => input.Where(s => !string.IsNullOrEmpty(s))
-        .Select(JArray.Parse).Chunk(2)
-        .Select((pair, index) => (result: comparer.Compare(pair[0], pair[1]), index: index + 1))
-        .Where(item => item.result > 0)
+        .Select(Parse.AsJsonElement).PairWise()
+        .Select((pair, index) => (result: comparer.Compare(pair.a, pair.b), index: index + 1))
+        .Where(item => item.result <= 0)
         .Select(item => item.index)
-        .ToList()
         .Sum();
 
-    static JToken[] dividers = new[]
-    {
-        JArray.Parse("[[2]]"),
-        JArray.Parse("[[6]]")
-    };
+    static readonly JsonElement[] dividers = "[ [[2]], [[6]] ]".AsJsonElement().ChildArray();
 
     public int Part2() => input.Where(s => !string.IsNullOrEmpty(s))
-        .Select(JArray.Parse)
+        .Select(Parse.AsJsonElement)
         .Concat(dividers)
         .ToImmutableSortedSet(comparer)
-        .Reverse()
         .Select((p, i) => (p, index: i + 1))
         .Join(dividers, outer => outer.p, p => p, (outer, _) => outer.index)
         .Aggregate(1, (l, r) => l * r);
 
 }
-
-class JTokenComparer : IComparer<JToken>
+static class Parse
 {
-    public int Compare(JToken? left, JToken? right) => (left!.Type, right!.Type) switch
+    public static JsonElement AsJsonElement(this string s) => JsonDocument.Parse(s).RootElement;
+    public static JsonElement[] ChildArray(this JsonElement e) => e.EnumerateArray().ToArray();
+}
+class MyJsonArrayComparer : IComparer<JsonElement>
+{
+    public int Compare(JsonElement left, JsonElement right) => (left.ValueKind, right.ValueKind) switch
     {
-        (JTokenType.Array, _) or (_, JTokenType.Array) => Compare(AsArray(left), AsArray(right)),
-        (JTokenType.Integer, JTokenType.Integer) => right.Value<int>().CompareTo(left.Value<int>()),
+        (JsonValueKind.Array, _) or (_, JsonValueKind.Array) => Compare(AsArray(left), AsArray(right)),
+        (JsonValueKind.Number, JsonValueKind.Number) => left.GetInt32().CompareTo(right.GetInt32()),
         _ => 0
     };
 
-    int Compare(JArray l, JArray r)
-    {
-        var left = l.Children().ToArray();
-        var right = r.Children().ToArray();
+    int Compare(JsonElement[] left, JsonElement[] right) => (
+        from p in left.Zip(right)
+        let result = Compare(p.First, p.Second)
+        where result != 0
+        select result as int?
+        ).FirstOrDefault() ?? left.Length.CompareTo(right.Length);
 
-        for (int i = 0; i < Math.Min(left.Length, right.Length); i++)
-        {
-            var result = Compare(left[i], right[i]);
-            if (result != 0)
-            {
-                return result;
-            }
-        }
-        return right.Length.CompareTo(left.Length);
-    }
-
-    static JArray AsArray(JToken token) => token.Type switch
+    static JsonElement[] AsArray(JsonElement token) => token.ValueKind switch
     {
-        JTokenType.Array => token.Value<JArray>()!,
-        JTokenType.Integer => JArray.Parse($"[{token.Value<int>()}]"),
+        JsonValueKind.Array => token.ChildArray(),
+        JsonValueKind.Number => $"[{token.GetInt32()}]".AsJsonElement().ChildArray(),
         _ => throw new Exception("unexpected")
     };
 }
