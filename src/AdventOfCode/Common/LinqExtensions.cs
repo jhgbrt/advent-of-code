e066e25606fa92ff5054f065770466e3092ff6b0
jhgbrt <jeroen@haegebaert.com>
2022-12-18 00:30:28 +0100

    day 17

--- src/AdventOfCode/Common/LinqExtensions.cs
+++ src/AdventOfCode/Common/LinqExtensions.cs
@@ -95,6 +95,36 @@ static class LinqExtensions
                 buffer.Dequeue();
             }
         }
+    }
 
+    public static (T a, T b) ToTuple2<T>(this T[] items)
+    {
+        if (items.Length != 2) throw new ArgumentException("Expected 2 items in array");
+        return (items[0], items[1]);
+    }
+    public static (T a, T b, T c) ToTuple3<T>(this T[] items)
+    {
+        if (items.Length != 3) throw new ArgumentException("Expected 3 items in array");
+        return (items[0], items[1], items[2]);
+    }
+    public static (T a, T b, T c, T d) ToTuple4<T>(this T[] items)
+    {
+        if (items.Length != 4) throw new ArgumentException("Expected 4 items in array");
+        return (items[0], items[1], items[2], items[3]);
+    }
+    public static (T a, T b, T c, T d, T e) ToTuple5<T>(this T[] items)
+    {
+        if (items.Length != 5) throw new ArgumentException("Expected 5 items in array");
+        return (items[0], items[1], items[2], items[3], items[4]);
+    }
+    public static (T a, T b, T c, T d, T e, T f) ToTuple6<T>(this T[] items)
+    {
+        if (items.Length != 6) throw new ArgumentException("Expected 6 items in array");
+        return (items[0], items[1], items[2], items[3], items[4], items[5]);
+    }
+    public static (T a, T b, T c, T d, T e, T f, T g) ToTuple7<T>(this T[] items)
+    {
+        if (items.Length != 7) throw new ArgumentException("Expected 7 items in array");
+        return (items[0], items[1], items[2], items[3], items[4], items[5], items[6]);
     }
 }
--- src/AdventOfCode/Common/Point.cs
+++ src/AdventOfCode/Common/Point.cs
@@ -4,4 +4,6 @@ readonly record struct Point(int x, int y)
 {
     public int ManhattanDistance(Point o) => Math.Abs(x - o.x) + Math.Abs(y - o.y);
     public override string ToString() => $"({x},{y})";
+
+    public static Point operator+(Point left, Point right) => new(left.x + right.x, left.y + right.y);
 }
