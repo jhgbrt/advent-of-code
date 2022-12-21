f8e3fc24c608bb2c68ce5ae58e4f31c44b1e6fa1
jhgbrt <jeroen@haegebaert.com>
2022-12-21 18:55:28 +0100

    day 21

--- src/AdventOfCode/Year2022/Day21/AoC.cs
+++ src/AdventOfCode/Year2022/Day21/AoC.cs
@@ -3,13 +3,14 @@ using AdventOfCode.Common;
 namespace AdventOfCode.Year2022.Day21;
 public class AoC202221
 {
+    static readonly bool usesample = true;
     static string[] sample = Read.SampleLines();
     static string[] input = Read.InputLines();
 
     static readonly Regex operationRegex = new Regex(@"(?<name>[a-z]+): (?<left>[a-z]+) (?<operation>.) (?<right>[a-z]+)", RegexOptions.Compiled);
     static readonly Regex numberRegex = new Regex(@"(?<name>[a-z]+): (?<number>[\d]+)", RegexOptions.Compiled);
     static IReadOnlyDictionary<string, Operation> instructions = (
-            from line in input
+            from line in usesample ? sample : input
             select operationRegex.As<MathOperation>(line) as Operation ?? numberRegex.As<YellNumber>(line) as Operation
             ).ToDictionary(o => o.name);
 
@@ -18,26 +19,83 @@ public class AoC202221
         where i.Value is MathOperation m
         let m = (MathOperation) i.Value
         from child in (m.left, m.right).AsEnumerable()
-        let parent = (MathOperation)instructions[child] 
+        let parent = m 
         select (child, parent)
         ).ToDictionary(x => x.child, x => x.parent);
 
     public long Part1() => GetValue("root", instructions);
-    public long Part2()
+    public long Part2() 
     {
 
-        var root = (MathOperation)instructions["root"];
-        var (left, right) = (root.left, root.right);
+        var queue = new Queue<string>();
 
-        return -1;
+        queue.Enqueue("humn");
+
+        var newinstructions = new Dictionary<string, Operation>();
+        while (queue.Any())
+        {
+            var name = queue.Dequeue();
+
+            Operation op = instructions[name] switch
+            {
+                YellNumber n when name is not "humn" => n,
+                _ => Transform(name)
+            };
+
+            newinstructions[op.name] = op;
+
+            var children = op switch
+            {
+                MathOperation m => (m.left, m.right).AsEnumerable(),
+                _ => Empty<string>()
+            };
+            
+            foreach (var c in children)
+            {
+                queue.Enqueue(c);
+            }
+        }
+
+
+        foreach (var item in newinstructions)
+        {
+            Console.WriteLine(item);
+        }
+
+        //return GetValue2("humn", instructions);
+        return 0;
     }
 
-    MathOperation Transform(string leaf, IReadOnlyDictionary<string, Operation> instructions, IDictionary<string, Operation> result)
+
+    long GetValue2(string name, IReadOnlyDictionary<string, Operation> instructions)
     {
-        return parents[leaf] switch
+        var operation = name switch
         {
-            MathOperation {operation: '+' } p when p.left == leaf => new MathOperation(p.left, p.name, '-', p.right),
-            MathOperation {operation: '+' } p when p.right == leaf => new MathOperation(p.right, p.name, '-', p.left),
+            "humn" => Transform(name),
+            _ => instructions[name]
+        };
+        Console.WriteLine($"GetValue2: {operation}");
+        return operation switch
+        {
+            YellNumber n => n.number,
+            _ => Transform(operation.name) switch
+            {
+                MathOperation { operation: '+' } m => GetValue2(m.left, instructions) + GetValue2(m.right, instructions),
+                MathOperation { operation: '-' } m => GetValue2(m.left, instructions) - GetValue2(m.right, instructions),
+                MathOperation { operation: '*' } m => GetValue2(m.left, instructions) * GetValue2(m.right, instructions),
+                MathOperation { operation: '/' } m => GetValue2(m.left, instructions) / GetValue2(m.right, instructions),
+                _ => throw new Exception()
+            }
+        };
+
+    }
+
+    MathOperation Transform(string leaf)
+    {
+        var transformed = parents[leaf] switch
+        {
+            MathOperation { operation: '+' } p when p.left == leaf => new MathOperation(p.left, p.name, '-', p.right),
+            MathOperation { operation: '+' } p when p.right == leaf => new MathOperation(p.right, p.name, '-', p.left),
             MathOperation { operation: '-' } p when p.left == leaf => new MathOperation(p.left, p.name, '+', p.right),
             MathOperation { operation: '-' } p when p.right == leaf => new MathOperation(p.right, p.left, '-', p.name),
             MathOperation { operation: '*' } p when p.left == leaf => new MathOperation(p.left, p.name, '/', p.right),
@@ -47,24 +105,24 @@ public class AoC202221
             _ => throw new Exception()
         };
 
+        Console.WriteLine($"{leaf}: {instructions[leaf]} -> {transformed}");
+
+        return transformed;
     }
 
-    long GetValue(string operation, IReadOnlyDictionary<string, Operation> instructions)
+    long GetValue(string operation, IReadOnlyDictionary<string, Operation> instructions) => instructions[operation] switch
     {
-        return instructions[operation] switch
+        YellNumber n => n.number,
+        MathOperation m => m.operation switch
         {
-            YellNumber n => n.number,
-            MathOperation m => m.operation switch
-            {
-                '+' => GetValue(m.left, instructions) + GetValue(m.right, instructions),
-                '-' => GetValue(m.left, instructions) - GetValue(m.right, instructions),
-                '*' => GetValue(m.left, instructions) * GetValue(m.right, instructions),
-                '/' => GetValue(m.left, instructions) / GetValue(m.right, instructions),
-                _ => throw new Exception()
-            },
+            '+' => GetValue(m.left, instructions) + GetValue(m.right, instructions),
+            '-' => GetValue(m.left, instructions) - GetValue(m.right, instructions),
+            '*' => GetValue(m.left, instructions) * GetValue(m.right, instructions),
+            '/' => GetValue(m.left, instructions) / GetValue(m.right, instructions),
             _ => throw new Exception()
-        };
-    }
+        },
+        _ => throw new Exception()
+    };
 
 }
 
