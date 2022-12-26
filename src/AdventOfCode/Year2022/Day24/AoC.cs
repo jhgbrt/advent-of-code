5ad4cd1e3e34bcc5aec28b62adbd78012b5fc20d
jhgbrt <jeroen@haegebaert.com>
2022-12-26 01:40:08 +0100

    day 24 part 1

--- src/AdventOfCode/Year2022/Day24/AoC.cs
+++ src/AdventOfCode/Year2022/Day24/AoC.cs
@@ -1,5 +1,7 @@
 using AdventOfCode.Common;
 
+using System.Security.Cryptography;
+
 namespace AdventOfCode.Year2022.Day24;
 public class AoC202224
 {
@@ -7,55 +9,50 @@ public class AoC202224
     static string[] sample = Read.SampleLines();
     static string[] input = Read.InputLines();
     static string[] theinput = usesample ? sample : input;
-    public object Part1()
+
+    Coordinate start = new Coordinate(1, 0);
+    Coordinate target = new Coordinate(theinput[0].Length - 2, theinput.Length - 1);
+    Grid grid = new Grid(theinput);
+
+    public object Part1() => FindPath(grid, start, target);
+
+
+    public object Part2()
     {
-        var start = new Coordinate(1, 0);
-        var target = new Coordinate(theinput[0].Length - 2, theinput.Length - 1);
-        var grid = new Grid(theinput);
-        return FindPath(grid, new(), new(1,0), target);
+        var t1 = FindPath(grid, start, target, 0);
+        var t2 = FindPath(grid, target, start, t1);
+        var t3 = FindPath(grid, start, target, t1 + t2);
+        Console.WriteLine((t1, t2, t3));
+        return t1 + t2 + t3;
     }
 
-
-    private (int, bool) FindPath(Grid grid, HashSet<(Coordinate, string)> seen, Coordinate current, Coordinate target)
+    int FindPath(Grid grid, Coordinate start, Coordinate destination, int t0 = 0)
     {
-        var key = (current, grid.ToString());
+        var cache = new Dictionary<Coordinate, int>();
 
-        if (seen.Contains(key))
-            return (int.MaxValue, false);
-
-        seen.Add(key);
-
-        int n = 1;
-        grid.Draw(current);
-        grid = grid.Move();
-
-        while (!grid.OpenPositions(current).Any())
+        cache[start] = 0;
+        int t = 0;
+        while (!cache.ContainsKey(destination))
         {
-            n++;
-            grid.Draw(current);
-            grid = grid.Move();
-        }
-        var reached = false;
-        int best = int.MaxValue;
-        foreach (var c in grid.OpenPositions(current))
-        {
-            if (c == target)
+            var blocked = grid.BlockedAt(t0 + t + 1);
+
+            var updated = (
+                from p in grid.Points()
+                where cache.TryGetValue(p, out var x) && x == t
+                from n in p.AsEnumerable().Concat(p.Neighbours())
+                where !blocked.Contains(n)
+                select n
+            ).ToList();
+
+            foreach (var item in updated)
             {
-                n += 1;
-                reached = true;
+                cache[item] = t + 1;
             }
-            else
-            {
-                (int i, reached) = FindPath(grid, seen, c, target);
-                if (reached) 
-                    n += i;
-            }
-            if (reached && best > n) best = n;
+            t++;
         }
-        return (best, reached);
+        return cache[destination];
     }
 
-    public object Part2() => "";
 }
 
 /// <summary>
@@ -70,12 +67,12 @@ class Grid
     //   |
     // y v
 
-    readonly ImmutableDictionary<Coordinate, List<char>> items;
+    readonly ImmutableDictionary<Coordinate, char> items;
     readonly Coordinate origin = new(0, 0);
     readonly Coordinate bottomright;
     readonly char empty;
-    int Bottom => bottomright.y;
-    int Right => bottomright.x;
+    public int Bottom => bottomright.y;
+    public int Right => bottomright.x;
     int Left => origin.x;
     int Top => origin.y;
 
@@ -84,57 +81,53 @@ class Grid
         bottomright = new(input[0].Length - 1, input.Length - 1);
         items = (from y in Range(0, input.Length)
                  from x in Range(0, input[y].Length)
-                 select (x, y, c: input[y][x]))
-                 .ToImmutableDictionary(t => new Coordinate(t.x, t.y), t => t.c.AsEnumerable().ToList());
+                 let c = input[y][x]
+                 where c != '.' || (x == 0 || y == 0 || x == Right || y == Bottom)
+                 select (x, y, c))
+                 .ToImmutableDictionary(t => new Coordinate(t.x, t.y), t => t.c);
+        
+      
         this.empty = empty;
     }
-    public Grid Move()
+
+
+    public ImmutableHashSet<Coordinate> BlockedAt(int t)
     {
-        var blizzard = (from item in items
-                        let c = item.Key
-                        from v in item.Value
-                        where v is '>' or '<' or 'v' or '^'
-                        let n = v switch
-                        {
-                            '>' => c with { x = c.x == Right - 1 ? Left + 1 : c.x + 1 },
-                            '<' => c with { x = c.x == Left + 1 ? Right - 1 : c.x - 1 },
-                            '^' => c with { y = c.y == Top + 1 ? Bottom - 1 : c.y - 1 },
-                            'v' => c with { y = c.y == Bottom - 1 ? Top + 1 : c.y + 1 },
-                            _ => c
-                        }
-                        group v by n).ToDictionary(g => g.Key, g => g.ToList());
-
-        var newitems = (
-            from y in Range(0, Bottom + 1)
-            from x in Range(0, Right + 1)
-            let c = new Coordinate(x, y)
-            let v = blizzard.ContainsKey(c)
-            ? blizzard[c]
-            : items[c] is ['#']
-            ? '#'.AsEnumerable()
-            : empty.AsEnumerable()
-            select (c, v))
-            .ToImmutableDictionary(t => t.c, t => t.v.ToList());
-
-        return new Grid(newitems, empty, bottomright);
-
+        return (
+            from kv in items
+            let c = kv.Key
+            let v = kv.Value
+            where v is not '.'
+            select v switch
+            {
+                '#' => c,
+                '>' => c with { x = Mod(c.x - 1 + t, Right - 1) + 1 },
+                '<' => c with { x = Mod(c.x - 1 - t, Right - 1) + 1 },
+                'v' => c with { y = Mod(c.y - 1 + t, Bottom - 1) + 1 },
+                '^' => c with { y = Mod(c.y - 1 - t, Bottom - 1) + 1 }
+            }
+            ).ToImmutableHashSet();
     }
+    public bool Contains(Coordinate n) => items.ContainsKey(n);
+    static T Mod<T>(T n, T m) where T : INumber<T> => (n % m + m) % m;
+
+   
 
     public IEnumerable<Coordinate> OpenPositions(Coordinate c)
         => from n in c.Neighbours()
-           where items.TryGetValue(n, out var v) && v is ['.']
+           where items.TryGetValue(n, out var v) && v is '.'
            select n;
 
-    private Grid(ImmutableDictionary<Coordinate, List<char>> items, char empty, Coordinate bottomright)
+    private Grid(ImmutableDictionary<Coordinate, char> items, char empty, Coordinate bottomright)
     {
         this.items = items;
         this.empty = empty;
         this.bottomright = bottomright;
     }
 
-    IReadOnlyList<char> this[Coordinate p] => items.TryGetValue(p, out var c) ? c : empty.AsEnumerable().ToList();
-    IReadOnlyList<char> this[(int x, int y) p] => this[new Coordinate(p.x,p.y)];
-    IReadOnlyList<char> this[int x, int y] => this[new Coordinate(x, y)];
+    public char this[Coordinate p] => items.TryGetValue(p, out var c) ? c : '.';
+    char this[(int x, int y) p] => this[new Coordinate(p.x,p.y)];
+    char this[int x, int y] => this[new Coordinate(x, y)];
 
     public IEnumerable<Coordinate> Points() =>
         from y in Range(origin.y, bottomright.y)
@@ -160,8 +153,7 @@ class Grid
                 }
                 else
                 {
-                    var l = this[x, y];
-                    var c = l.Count() == 1 ? l[0] : l.Count().ToString()[0];
+                    var c = this[x, y];
                     Console.Write(c);
                 }
             }
@@ -176,8 +168,7 @@ class Grid
         {
             for (int x = Left; x <= Right; x++)
             {
-                var l = this[x, y];
-                var c = l.Count() == 1 ? l[0] : l.Count().ToString()[0];
+                var c = this[x, y];
                 sb.Append(c); 
             }
             sb.AppendLine();
