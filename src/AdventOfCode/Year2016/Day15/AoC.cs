ee28d3ca8b68c0d0a6e2d842e659407f68fc28a1
jhgbrt <jeroen@haegebaert.com>
2022-12-29 17:23:23 +0100

    2016 15, 16

--- src/AdventOfCode/Year2016/Day15/AoC.cs
+++ src/AdventOfCode/Year2016/Day15/AoC.cs
@@ -2,6 +2,17 @@ namespace AdventOfCode.Year2016.Day15;
 
 public class AoC201615
 {
-    public object Part1() => "";
-    public object Part2() => "";
+    static Regex AoC201615Regex = new(@"^Disc #(?<id>\d+) has (?<positions>\d+) positions; at time=0, it is at position (?<position>\d+)\.$");
+    static string[] input = Read.InputLines();
+    static ImmutableArray<Disc> discs = (from line in input
+                                         select AoC201615Regex.As<Disc>(line)).ToImmutableArray();
+
+    static ImmutableArray<Disc> discs2 = discs.Add(new Disc(discs.Max(d => d.id) + 1, 11, 0));
+    public int Part1() => Range(0, int.MaxValue).First(t => discs.All(d => d.Position(t) == 0));
+    public int Part2() => Range(0, int.MaxValue).First(t => discs2.All(d => d.Position(t) == 0));
+}
+
+readonly record struct Disc(int id, int positions, int position)
+{
+    public int Position(int time) => (time + position + id) % positions;
 }
--- src/AdventOfCode/Year2016/Day16/AoC.cs
+++ src/AdventOfCode/Year2016/Day16/AoC.cs
@@ -2,6 +2,43 @@ namespace AdventOfCode.Year2016.Day16;
 
 public class AoC201616
 {
-    public object Part1() => "";
-    public object Part2() => "";
+    string input = Read.InputLines()[0];
+    public object Part1()
+    {
+        var data = GenerateData(input, 272);
+        return CalculateChecksum(data);
+    }
+    public object Part2()
+    {
+        var data = GenerateData(input, 35651584);
+        return CalculateChecksum(data);
+    }
+    private static string GenerateData(string input, int required)
+    {
+        var a = input;
+        while (a.Length < required)
+        {
+            var b = new string(a.Reverse().ToArray()).Replace('1', '-').Replace('0', '1').Replace('-', '0');
+            a = $"{a}0{b}";
+        }
+        return a.Substring(0, required);
+    }
+
+    private static string CalculateChecksum(string input)
+    {
+        string checksum = input;
+        var sb = new StringBuilder();
+        while (checksum.Length % 2 == 0)
+        {
+            sb.Clear();
+            for (var i = 0; i < checksum.Length - 1; i += 2)
+            {
+                sb.Append(checksum[i] == checksum[i + 1] ? '1' : '0');
+            }
+            checksum = sb.ToString();
+        }
+        return checksum;
+    }
+
+ 
 }
