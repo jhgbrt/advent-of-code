cedc177d6c38eba2a9f4f072aa6e28e94d4de8ef
jhgbrt <jeroen@haegebaert.com>
2022-12-29 23:55:10 +0100

    2016 day 19

--- src/AdventOfCode/Year2016/Day19/AoC.cs
+++ src/AdventOfCode/Year2016/Day19/AoC.cs
@@ -2,6 +2,26 @@ namespace AdventOfCode.Year2016.Day19;
 
 public class AoC201619
 {
-    public object Part1() => "";
-    public object Part2() => "";
+    int input = 3001330;
+
+    public object Part1()
+    {
+        int n = input;
+        var p = 1;
+        while (p < n)
+        {
+            p *= 2;
+        }
+        if (p == n) return 1;
+        return 2 * n - p + 1;
+    }
+
+    public object Part2()
+    {
+        int n = input;
+        int p = 1;
+        while (p*3 < n)
+            p *= 3;
+        return n - p;
+    }
 }
