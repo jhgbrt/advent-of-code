d0c3679022bca380e6cbb938b670085d13ea629f
jhgbrt <jeroen@haegebaert.com>
2022-12-29 15:53:06 +0100

    2016 14

--- src/AdventOfCode/Year2016/Day14/AoC.cs
+++ src/AdventOfCode/Year2016/Day14/AoC.cs
@@ -6,102 +6,62 @@ namespace AdventOfCode.Year2016.Day14;
 public class AoC201614
 {
     //readonly string salt = "abc";
-    readonly string salt = "zpqevtbw";
+    readonly string salt = Read.InputLines()[0];
     string CurrentPath([CallerFilePath] string path = "") => Path.GetDirectoryName(path) ?? "";
-    public object Part1()
-    {
-        Dictionary<int, string> hashes = new();
-        var q = (
-            from i in Range(1, int.MaxValue)
-            let repeat = Find3(hashes, salt, i)
-            where repeat.HasValue
-            from j in Range(i + 1, 1000).SkipWhile(j => !IsValid2(hashes, salt, j, repeat.Value)).Take(1)
-            select i
-            ).Take(64);
-
-        {
-            int n = 1;
-            int index = 0;
-            foreach (var item in q)
-            {
-                Console.WriteLine($"{n++}: {item}");
-                index = item;
-            }
-            return index;
-        }
-    }
-
-    private int Write(int i,  char c, int j)
-    {
-        Console.WriteLine($"{i} - {c} - {j}");
-        return i;
-    }
-
     static MD5 md5 = MD5.Create();
 
-    static string GetHash(Dictionary<int, string> hashes, string salt, int seed, int repeat = 0)
+    public int Part1() => FindHash64(GetHashes(50000, 0));
+
+    public int Part2() => FindHash64(GetHashes(50000, 2016));
+
+    public int FindHash64(IReadOnlyDictionary<int, string> hashes) => (
+            from i in Range(1, int.MaxValue)
+            let repeat = Find3(hashes[i])
+            where repeat.HasValue
+            from j in Range(i + 1, 1000).SkipWhile(j => !IsValid(hashes[j], repeat.Value)).Take(1)
+            select i
+            ).Take(64).Last();
+
+    IReadOnlyDictionary<int,string> GetHashes(int n, int repeat)
     {
-        if (!hashes.ContainsKey(seed))
+        var path = Path.Combine(CurrentPath(), $"hashes-{salt}-{repeat}.txt");
+        if (!File.Exists(path))
         {
-            var key = $"{salt}{seed}";
-            var hash = md5.ComputeHash(Encoding.ASCII.GetBytes(key));
-            for (int i = 0; i < repeat; i++)
+            using var s = new StreamWriter(File.OpenWrite(path));
+            foreach (var i in Range(0, n))
             {
-                var bytes = Encoding.ASCII.GetBytes(ToString(hash).ToLower());
-                hash = md5.ComputeHash(bytes);
+                var hash = ComputeHash(salt, i, repeat);
+                s.WriteLine(hash);
             }
-            var hashStr = ToString(hash).ToLower();
-            char? three = null;
-            for (int i = 0; i < hashStr.Length - 3; i++)
-            {
-                if (hashStr[i] == hashStr[i + 1] && hashStr[i] == hashStr[i+2])
-                {
-                    three = hashStr[i];
-                    //Console.WriteLine($"{hashStr} has {three}{three}{three} at {i}");
-                    break;
-                }
-            }
-            var fives = new List<char>();
-            for (int i = 0; i < hashStr.Length - 5; i++)
-            {
-                if (hashStr[i] == hashStr[i + 1] && hashStr[i] == hashStr[i + 2] && hashStr[i] == hashStr[i+3] && hashStr[i] == hashStr[i+4])
-                {
-                    fives.Add(hashStr[i]);
-                    var c = hashStr[i];
-                    Console.WriteLine($"{hashStr} has {c}{c}{c}{c}{c} at {i}");
-                    break;
-                }
-            }
-            hashes[seed] = hashStr;
         }
-        return hashes[seed];
+        return File.ReadLines(path).Select((line, i) => (line, i)).ToDictionary(x => x.i, x => x.line);
+    }
+    
+    static string ComputeHash(string salt, int seed, int repeat = 0)
+    {
+        var key = $"{salt}{seed}";
+        var hash = md5.ComputeHash(Encoding.ASCII.GetBytes(key));
+        for (int i = 0; i < repeat; i++)
+        {
+            var bytes = Encoding.ASCII.GetBytes(ToString(hash).ToLower());
+            hash = md5.ComputeHash(bytes);
+        }
+        return ToString(hash).ToLower();
     }
 
-    static char? Find3(Dictionary<int, string> hashes, string salt, int seed, int repeat = 0)
-    {
-        var hash = GetHash(hashes, salt, seed, repeat).AsSpan();
-        for (int i = 0; i < hash.Length - 3; i++)
-        {
-            var c = hash[i];
-            if (hash[i + 1] == c && hash[i + 2] == c) return c;
-        }
-        return null;
-
-    }
-    static bool IsValid2(Dictionary<int, string> hashes, string salt, int seed, char c, int repeat = 0)
-    {
-        var hash = GetHash(hashes, salt, seed, repeat);
-        for (int i = 0; i < hash.Length - 5; i++)
-        {
-            if (hash[i] == c
-                && hash[i + 1] == c
-                && hash[i + 2] == c
-                && hash[i + 3] == c
-                && hash[i + 4] == c
-                ) return true;
-        }
-        return false;
-    }
+    static char? Find3(string hash) => (from i in Range(0, hash.Length - 2)
+                                        let a = hash[i]
+                                        let b = hash[i+1]
+                                        where a == b
+                                        let c = hash[i+2]
+                                        where c == a
+                                        select (char?)a).FirstOrDefault();
+    static bool IsValid(string hash, char c) 
+        => Range(0, hash.Length - 4).Any(i => hash[i] == c
+                                        && hash[i + 1] == c
+                                        && hash[i + 2] == c
+                                        && hash[i + 3] == c
+                                        && hash[i + 4] == c);
 
     static string ToString(byte[] hash)
     {
@@ -113,27 +73,5 @@ public class AoC201614
         return sb.ToString();
     }
 
-    public object Part2()
-    {
-        Dictionary<int, string> hashes = new();
-        var path = Path.Combine(CurrentPath(), "hashes-extended.txt");
-        if (!File.Exists(path))
-        {
-            using var s = new StreamWriter(File.OpenWrite(path));
-            foreach (var i in Range(0, 50000))
-            {
-                var hash = GetHash(hashes, salt, i, 2016);
-                s.WriteLine(hash);
-            }
-        }
-
-        var q = (
-            from i in Range(1, int.MaxValue)
-            let repeat = Find3(hashes, salt, i, 2016)
-            where repeat.HasValue
-            from j in Range(i + 1, 1000).SkipWhile(j => !IsValid2(hashes, salt, j, repeat.Value, 2016)).Take(1)
-            select i
-            ).Take(64);
-        return q.Last();
-    }
+   
 }
