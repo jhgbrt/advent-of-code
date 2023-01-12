f5960af3b6876aadbd68579e1a70820c11350fa1
jhgbrt <jeroen@haegebaert.com>
2023-01-13 00:50:47 +0100

    2018 complete

--- src/AdventOfCode/Year2018/Day25/AoC.cs
+++ src/AdventOfCode/Year2018/Day25/AoC.cs
@@ -2,6 +2,52 @@ namespace AdventOfCode.Year2018.Day25;
 
 public static class AoC201825
 {
-    public static object Part1() => "";
+    static string[] input = Read.InputLines();
+    static Point[] points = (
+        from line in input
+        let ints = (int[])MyConvert.ChangeType(line, typeof(int[]), new CsvLineFormatInfo())
+        select new Point(ints[0], ints[1], ints[2], ints[3])
+        ).ToArray();
+
+    public static int Part1()
+    {
+        var remaining = points.ToHashSet();
+        List<HashSet<Point>> constellations = new();
+
+        while (remaining.Any())
+        {
+            var first = remaining.First();
+            var constellation = new HashSet<Point>
+            {
+                first
+            };
+            remaining.Remove(first);
+
+            bool found = false;
+            do
+            {
+                found = false;
+                foreach (var r in remaining.ToList())
+                {
+                    if (constellation.Any(p => p.ManhattanDistance(r) <= 3))
+                    {
+                        constellation.Add(r);
+                        remaining.Remove(r);
+                        found = true;
+                    }
+                }
+            } while (found);
+            constellations.Add(constellation);
+        }
+
+        return constellations.Count;
+    }
+
     public static object Part2() => "";
+}
+
+readonly record struct Point(int x, int y, int z, int w)
+{
+    public int ManhattanDistance(Point other)
+        => Abs(x - other.x) + Abs(y - other.y) + Abs(z - other.z) + Abs(w - other.w);
 }
