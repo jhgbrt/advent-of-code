01a6a9fdbc7b14553f5eb772fbb21dbaf475fc07
jhgbrt <jeroen@haegebaert.com>
2023-12-04 19:46:34 +0100

    cleanup

--- src/AdventOfCode/Year2023/Day04/aoc.cs
+++ src/AdventOfCode/Year2023/Day04/aoc.cs
@@ -9,19 +9,18 @@ public class AoC202304
                               select Pow(2, e - 1)).Sum();
     public object Part2() 
     {
-        var cards = items.ToDictionary(c => c.Id);
-        for (int i = 1; i <= cards.Count; i++)
+        var cards = items.ToList();
+        foreach (var card in cards)
         {
-            var card = cards[i];
-            for (var c = card.Processed; c <= card.Count; c++)
+            for (var c = 0; c < card.Count; c++)
             {
-                foreach (var offset in Range(1, card.NofWinning))
+                foreach (var offset in Range(0, card.NofWinning))
                 {
                     cards[card.Id + offset].Count++;
                 }
             }
         }
-        return cards.Sum(c => c.Value.Count);
+        return cards.Sum(c => c.Count);
     }
 }
 
@@ -33,31 +32,24 @@ class Card
         this.id = id;
         nofwinning = winning.Count(numbers.Contains);
         Count = 1;
-        Processed = 1;
     }
-    public int Processed{ get; set; }
     public int Count { get; set; }
     public int Id => id;
 
     public int NofWinning => nofwinning;
     public static Card Parse(string s)
     {
-        //var match = Regexes.MyRegex().Match(s);
-        //var id = int.Parse(match.Groups["id"].Value); 
-        //var winning = match.Groups["winning"].Value.Split(' ').Select(int.Parse).ToArray();
-        //var numbers = match.Groups["numbers"].Value.Split(' ').Select(int.Parse).ToArray();
-        var split1 = s.Split(':', StringSplitOptions.TrimEntries);
-        var split2 = split1[1].Split("|", StringSplitOptions.TrimEntries);
-        var id = int.Parse(split1[0].Split(' ').Last());
-        var winning = split2[0].Split(' ', StringSplitOptions.TrimEntries|StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-        var numbers = split2[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        var match = Regexes.MyRegex().Match(s);
+        var id = int.Parse(match.Groups["id"].Value);
+        var winning = match.Groups["winning"].Value.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        var numbers = match.Groups["numbers"].Value.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
         return new(id, winning, numbers);
     }
 }
 
 static partial class Regexes
 {
-    [GeneratedRegex(@"^Card (?<id>\d.*): (?<winning>[^|]+) | (?<numbers>[^|]+)$")]
+    [GeneratedRegex(@"^Card +(?<id>\d.*): +(?<winning>[\d ]+) \| +(?<numbers>.*)")]
     public static partial Regex MyRegex();
 }
 
@@ -66,15 +58,17 @@ public class Tests
     [Fact]
     public void Can_Parse_Card()
     {
-        var input = "Card 1: 12 34 45 | 78 90 12 34 45";
-        var match = Regexes.MyRegex().Match(input);
+        var regex = Regexes.MyRegex();
+        var input = "Card  1: 12 34 45 |  8 90 12 34 45";
+        var match = regex.Match(input);
+        Assert.True(match.Success);
         var id = int.Parse(match.Groups["id"].Value);
         Assert.Equal(1, id);
         
-        var winning = match.Groups["winning"].Value.Split(' ').Select(int.Parse).ToArray();
+        var winning = match.Groups["winning"].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries|StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
         Assert.Equal(new[] { 12, 34, 45 }, winning);
 
-        var numbers = match.Groups["numbers"].Value.Split(' ').Select(int.Parse).ToArray();
-        Assert.Equal(new[]{78,90,12,34,45}, numbers);
+        var numbers = match.Groups["numbers"].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+        Assert.Equal(new[] { 8,90,12,34,45}, numbers);
     }
 }
