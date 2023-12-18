effe79c1ad4d1a8b530ef7dcd4fc2acdd0fcb2c8
jhgbrt <jeroen@haegebaert.com>
2023-12-18 23:51:26 +0100

    day 18

--- src/AdventOfCode/Year2023/Day18/aoc.cs
+++ src/AdventOfCode/Year2023/Day18/aoc.cs
@@ -1,142 +1,70 @@
-using Spectre.Console;
-
-using System.Security.Cryptography;
 
 namespace AdventOfCode.Year2023.Day18;
+
+
+
 public class AoC202318
 {
     public AoC202318():this(Read.InputLines(), Console.Out) {}
     readonly TextWriter writer;
-    readonly ImmutableArray<Item> items;
-    internal IEnumerable<Item> Items => items;
+    string[] input;
+    readonly ImmutableArray<Item> items1;
+    readonly ImmutableArray<Item> items2;
+    internal IEnumerable<Item> Items1 => items1;
+    internal IEnumerable<Item> Items2 => items2;
     public AoC202318(string[] input, TextWriter writer)
     {
-        items = input.Select(s => Regexes.MyRegex().As<Item>(s)).ToImmutableArray();
+        this.input = input;
+        items1 = input.Select(s => Regexes.RegexPart1().As<Item>(s)).ToImmutableArray();
+        items2 = input.Select(s =>
+            {
+                var match = Regexes.RegexPart2().Match(s);
+                var distance = Convert.ToInt32(match.Groups["distance"].Value, 16);
+                var dir = match.Groups["dir"].Value[0] switch { '0' => 'R', '1' => 'D', '2' => 'L', '3' => 'U' };
+                return new Item(dir, distance);
+            }
+        ) .ToImmutableArray();
         this.writer = writer;
     }
 
-    public object Part1()
+    public long Part1() => Solve(items1);
+
+    public long Part2() => Solve(items2);
+
+    private long Solve(IEnumerable<Item> items)
     {
-        //var g = new InfiniteGrid();
-        var list = new List<Coordinate>();
-        var c = Coordinate.Origin;
-        list.Add(c);
-        foreach (var item in items)
+        var q = from item in items
+                select (item.distance,
+                    dx: item.direction switch { 'R' => item.distance, 'L' => -item.distance, _ => 0 },
+                    dy: item.direction switch { 'D' => item.distance, 'U' => -item.distance, _ => 0 }
+                );
+
+        // https://en.wikipedia.org/wiki/Pick%27s_theorem
+        // https://en.wikipedia.org/wiki/Shoelace_formula
+        var current = (x: 0L, y: 0L);
+        var (area, perimeter) = (0L, 0L);
+        foreach (var (distance, dx, dy) in q)
         {
-           for (var i = 0; i < item.n; i++)
-           {
-               c = item.dir switch
-               {
-                   'R' => c.E,
-                   'L' => c.W,
-                   'U' => c.N,
-                   'D' => c.S
-               };
-               list.Add(c);
-           }
+            var next = (x: current.x + dx, y: current.y + dy);
+            area += (current.x * next.y) - (current.y * next.x);
+            perimeter += distance;
+            current = next;
         }
-
-        //var rows = (
-        //    from item in list
-        //    group item by item.y into g
-        //    let row = g.Key
-        //    let points = g.Select(_ => _.x).Order().ToList()
-        //    select points).ToList();
-
-        //foreach (var row in rows)
-        //{
-        //    var i = 0;
-        //    while (i < row.Count - 2)
-        //    {
-        //        while (row[i + 1] == row[i] + 1) i++;
-        //    }
-        //}
-
-        return -1;
-    }
-
-    internal IEnumerable<Range> Ranges(IList<int> row)
-    {
-        if (row[0] > 0)
-            yield return new(0, row[0]);
-        var start = row[0];
-        var previous = -1;
-        var enumerator = row.GetEnumerator();
-        while (enumerator.MoveNext())
-        {
-            var current = enumerator.Current;
-            if (current == previous + 1)
-            {
-                previous = current;
-            }
-            else if (previous > 0)
-            {
-                yield return new Range(start, previous + 1);
-                yield return new Range(previous + 1, current);
-                start = current;
-                previous = current;
-            }
-            else
-            {
-            }
-        }
-        yield return new(start, row[^1] + 1);
-    }
-
-    public object Part2() => "";
-
-    private bool IsInsideLoop(FiniteGrid grid, Coordinate p) => CountLinesCrossed(grid, p) % 2 == 0;
-
-    int CountLinesCrossed(FiniteGrid grid, Coordinate p)
-    {
-        Func<State, Coordinate, Result> f = NotOnLine;
-        var state = new State(grid, p, 0);
-        var e = East(grid, p).GetEnumerator();
-        while (e.MoveNext())
-        {
-            (f, state) = f(state, e.Current);
-        }
-        return state.Count;
-    }
-
-    static IEnumerable<Coordinate> East(FiniteGrid grid, Coordinate c)
-    {
-        var current = c;
-        while (current.x < grid.Width)
-        {
-            yield return current;
-            current = current.E;
-        }
-    }
-
-    Result NotOnLine(State state, Coordinate c) => state.Grid[c] switch
-    {
-        '#' => new(OnLine, state),
-        '.' => new(NotOnLine, state)
-    };
-
-    Result OnLine(State state, Coordinate c) => state.Grid[c] switch
-    {
-        '#' => new(OnLine, state),
-        '.' => new(NotOnLine, state.AddCount()),
-    };
-
-
-    record struct Result(Func<State, Coordinate, Result> Next, State State);
-
-    record struct State(FiniteGrid Grid, Coordinate p, int Count)
-    {
-        public State AddCount() => this with { Count = Count + 1 };
+        area = (area + current.x - current.y)/2;
+        area = area + (perimeter / 2) + 1;
+        return area;
     }
 
 }
 
-readonly record struct Item(char dir, int n, string color);
+readonly record struct Item(char direction, int distance);
 
 static partial class Regexes
 {
-    [GeneratedRegex(@"^(?<dir>[RLUD]) (?<n>\d+) \((?<color>).*\)$")]
-    public static partial Regex MyRegex();
+    [GeneratedRegex(@"^(?<direction>[RLUD]) (?<distance>\d+) .*$")]
+    public static partial Regex RegexPart1();
+    [GeneratedRegex(@"^[RLUD] \d+ \(#(?<distance>\w{5})(?<dir>\w)\)$")]
+    public static partial Regex RegexPart2();
 }
 
 public class AoC202318Tests
@@ -151,9 +79,10 @@ public class AoC202318Tests
     [Fact]
     public void TestParsing()
     {
-        Assert.Equal(14, sut.Items.Count());
-        Assert.Equal(6, sut.Items.First().n);
-        Assert.Equal('R', sut.Items.First().dir);
+        Assert.Equal(14, sut.Items1.Count());
+        Assert.Equal(6, sut.Items1.First().distance);
+        Assert.Equal('R', sut.Items1.First().direction);
+        Assert.Equal(461937, sut.Items2.First().distance);
     }
 
     [Fact]
@@ -165,27 +94,8 @@ public class AoC202318Tests
     [Fact]
     public void TestPart2()
     {
-        Assert.Equal(string.Empty, sut.Part2());
-    }
-
-    [Fact]
-    public void Ranges1()
-    {
-        var input = new[] { 0, 1, 2, 5, 6 };
-        var ranges = sut.Ranges(input);
-        Assert.Equal(new Range[] { new(0, 3), new(3, 5), new(5, 7) }, ranges);
-    }
-
-    [Fact]
-    public void Ranges2()
-    {
-        var input = new[] { 2, 3, 6 };
-        var ranges = sut.Ranges(input);
-        Assert.Equal(new Range[] { new(0, 2), new(2, 4), new(4, 7) }, ranges);
+        Assert.Equal(952408144115, sut.Part2());
     }
 
 }
 
-record struct Range(int start, int end)
-{
-}
