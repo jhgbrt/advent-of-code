d9c0eefc8315eb82a07d7d553642d8d9a45951be
Jeroen Haegebaert <jeroen@haegebaert.com>
2024-12-14 17:39:03 +0100

    scan inputs

---
+++ tools/scaninputs/Program.cs
@@ -0,0 +1,12 @@
+﻿
+foreach (var file in Directory.GetFiles(".", "input.txt", SearchOption.AllDirectories))
+{
+    var lines = File.ReadLines(file).Take(2).ToList();
+    if (lines.Count == 1 && lines[0].All(c => char.IsDigit(c) || c == ' '))
+    {
+        Console.WriteLine(file);
+        Console.WriteLine(lines[0]);
+    }
+}
+
+
