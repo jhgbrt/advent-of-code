821024325b512e292e53fb3c5b4cddd0a1bbaebe
Jeroen Haegebaert <jeroen.haegebaert@ae.be>
2025-12-01 16:37:02 +0100

    2025 01/2

--- src/AdventOfCode/Year2025/Day01/aoc.cs
+++ src/AdventOfCode/Year2025/Day01/aoc.cs
@@ -1,80 +1,54 @@
-using Microsoft.Build.Framework;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-
-using Sprache;
-
-using System.Linq;
-
 namespace AdventOfCode.Year2025.Day01;
 
-record struct Instruction(char direction, int distance);
-
-public class AoC202501(string[] input, TextWriter writer)
+public class AoC202501
 {
-    public AoC202501() : this(Read.InputLines(), Console.Out) 
+    int[] instructions;
+    public AoC202501(string[] input)
+    {
+        this.instructions = [.. input.Select(line => (line[0] == 'L' ? -1 : 1) * int.Parse(line[1..]))];
+    }
+
+    public AoC202501() : this(Read.InputLines()) 
     {
     }
 
-    Instruction[] instructions = input
-        .Select(line => new Instruction(line[0], int.Parse(line[1..])))
-        .ToArray();
-
-    public int Part1()
+    public int Part1() => instructions.Aggregate((value: 50, password: 0), (acc, i) =>
     {
-        var value = 50;
-        var password = 0;
-        foreach (var instr in instructions)
-        {
-            value = Mod(instr.direction switch
-            {
-                'L' => value - instr.distance,
-                _ => value + instr.distance
-            }, 100);
+        var next = Mod(acc.value + i, 100);
+        var increment = next == 0 ? 1 : 0;
+        return acc with { value = next, password = acc.password + increment };
+    }).password;
 
-            if (value == 0) password++;
-        }
-        return password; 
-    }
-    
+    public int Part2() => instructions.Aggregate((value: 50, password: 0), (acc, i) =>
+    {
+        var next = Mod(acc.value + i, 100);
+        var increment = i >= 0
+            ? (acc.value + i) / 100
+            : (-i - acc.value + (acc.value == 0 ? 0 : 100)) / 100;
+        return acc with { value = next, password = acc.password + increment };
+    }).password;
+
     private static int Mod(int value, int mod)
     {
         var result = value % mod;
         return result < 0 ? result + mod : result;
     }
-
-
-    public int Part2() => -1;
 }
 
 public class AoC202501Tests
 {
     private readonly AoC202501 sut;
-    public AoC202501Tests(ITestOutputHelper output)
+    public AoC202501Tests()
     {
         var input = Read.SampleLines();
-        sut = new AoC202501(input, new TestWriter(output));
+        sut = new AoC202501(input);
     }
 
     [Fact]
     public void TestParsing()
     {
-        Instruction[] instructions = Read.SampleLines()
-            .Select(line => new Instruction(line[0], int.Parse(line[1..])))
-            .ToArray();
-        var result = instructions.Aggregate(50, (value, instr) =>
-        Mod(instr.direction switch
-        {
-            'L' => value - instr.distance,
-            _ => value + instr.distance
-        }, 100) == 0 ? 1 : 0);
+    }
 
-        Assert.Equal(3, result);
-    }
-    private static int Mod(int value, int mod)
-    {
-        var result = value % mod;
-        return result < 0 ? result + mod : result;
-    }
     [Fact]
     public void TestPart1()
     {
@@ -84,6 +58,7 @@ public class AoC202501Tests
     [Fact]
     public void TestPart2()
     {
-        Assert.Equal(-1, sut.Part2());
+        Assert.Equal(6, sut.Part2());
     }
+
 }
