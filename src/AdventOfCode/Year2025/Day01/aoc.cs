7a1e02bbe9eb1344e7904cda2f546fc59bb84571
Jeroen Haegebaert <jeroen.haegebaert@ae.be>
2025-12-02 21:08:40 +0100

    fixed day1

--- src/AdventOfCode/Year2025/Day01/aoc.cs
+++ src/AdventOfCode/Year2025/Day01/aoc.cs
@@ -21,11 +21,16 @@ public class AoC202501
 
     public int Part2() => instructions.Aggregate((value: 50, password: 0), (acc, i) =>
     {
-        var next = (acc.value + 100 + i) % 100;
-        var increment = i >= 0
-            ? (acc.value + i) / 100
-            : (-i - acc.value + (acc.value == 0 ? 0 : 100)) / 100;
-        return acc with { value = next, password = acc.password + increment };
+        int dir = Math.Sign(i);
+        int steps = Math.Abs(i);
+        int count = 0;
+        for (int k = 1; k <= steps; k++)
+        {
+            int next = ((acc.value + k * dir) % 100 + 100) % 100;
+            if (next == 0) count++;
+        }
+        int final = ((acc.value + steps * dir) % 100 + 100) % 100;
+        return (value: final, password: acc.password + count);
     }).password;
 }
 
@@ -53,6 +58,13 @@ public class AoC202501Tests
     public void TestPart2()
     {
         Assert.Equal(6, sut.Part2());
+    }
+
+    [Fact]
+    public void Part2RealInput()
+    {
+        var sut = new AoC202501(Read.InputLines());
+        Assert.Equal(6616, sut.Part2());
     }
 
 }
